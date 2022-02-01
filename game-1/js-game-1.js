8a4df24a4f61a486e274c3998c331795f4d1629f
Jemdou <69933295+jemdou@users.noreply.github.com>
2022-02-01 19:43:32 +0900

    Add files via upload

--- game-1/js-game-1.js
+++ game-1/js-game-1.js
@@ -1,6 +1,11 @@
 var game1_input;
 
-
+var u_hp = 10;
+var u_maxhp = 10;
+var u_mana = 10;
+var u_maxmana = 10;
+var u_stamina = 10;
+var u_maxstamina = 10;
 
 
 /* 
@@ -13,6 +18,15 @@ document.getElementById("input").addEventListener("keyup",
     }
 );
 */
+function showStats() {
+    document.getElementById("user_hp").innerHTML = u_hp;
+    document.getElementById("user_maxhp").innerHTML = u_maxhp;
+    document.getElementById("user_mana").innerHTML = u_mana;
+    document.getElementById("user_maxmana").innerHTML = u_maxmana;
+    document.getElementById("user_stamina").innerHTML = u_stamina;
+    document.getElementById("user_maxstamina").innerHTML = u_maxstamina;
+}
+
 
 function displayMessage() {
     game1_input = document.getElementById("input").value; 
@@ -22,4 +36,17 @@ function displayMessage() {
     else {
         document.getElementById("entered-input").innerHTML = "no input";
     }
+}
+
+function decreaseHp() {
+    u_hp -= 1;
+    document.getElementById("user_hp").innerHTML = u_hp;
+}
+function decreaseMana() {
+    u_mana -= 1;
+    document.getElementById("user_mana").innerHTML = u_mana;
+}
+function decreaseStamina() {
+    u_stamina -= 1;
+    document.getElementById("user_stamina").innerHTML = u_stamina;
 }
