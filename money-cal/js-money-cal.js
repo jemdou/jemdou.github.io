1afa7a14fdba76035688cd9b94d5370840df052f
Jemdou <69933295+jemdou@users.noreply.github.com>
2022-06-11 19:42:53 +0900

    Add files via upload

--- money-cal/js-money-cal.js
+++ money-cal/js-money-cal.js
@@ -6,10 +6,24 @@ var iphone13_mini_128 = false;
 var iphone13_pro_128 = false;
 var iphone13_promax_128 = false;
 
+var iphone12_reg_64 = false;
+var iphone12_mini_64 = false;
+
+var iphoneSE3_64 = false;
+var iphoneSE3_128 = false;
+
+var ipad_64 = false;
+var ipad_256 = false;
+
+var ipadAir_64 = false;
+var ipadAir_256 = false;
+
+
+
 
 var total = 0;
 
-
+//13
 function addiphone13_reg_128() {
     if(!iphone13_reg_128) {
         total += 109;
@@ -21,6 +35,91 @@ function addiphone13_reg_128() {
     }
     showResult();
 }
+function addiphone13_mini_128() {
+    if(!iphone13_mini_128) {
+        total += 95;
+        iphone13_mini_128 = true;
+    }
+    else {
+        total -= 95;
+        iphone13_mini_128 = false;
+    }
+    showResult();
+}
+function addiphone13_pro_128() {
+    if(!iphone13_pro_128) {
+        total += 135;
+        iphone13_pro_128 = true;
+    }
+    else {
+        total -= 135;
+        iphone13_pro_128 = false;
+    }
+    showResult();
+}
+function addiphone13_promax_128() {
+    if(!iphone13_promax_128) {
+        total += 149;
+        iphone13_promax_128 = true;
+    }
+    else {
+        total -= 149;
+        iphone13_promax_128 = false;
+    }
+    showResult();
+}
+
+//12
+function addiphone12_reg_64() {
+    if(!iphone12_reg_64) {
+        total += 95;
+        iphone12_reg_64 = true;
+    }
+    else {
+        total -= 95;
+        iphone12_reg_64 = false;
+    }
+    showResult();
+}
+function addiphone12_mini_64() {
+    if(!iphone12_mini_64) {
+        total += 85;
+        iphone12_mini_64 = true;
+    }
+    else {
+        total -= 85;
+        iphone12_mini_64 = false;
+    }
+    showResult();
+}
+
+//se
+function addiphoneSE3_64() {
+    if(!iphoneSE3_64) {
+        total += 59;
+        iphoneSE3_64 = true;
+    }
+    else {
+        total -= 59;
+        iphoneSE3_64 = false;
+    }
+    showResult();
+}
+function addiphoneSE3_128() {
+    if(!iphoneSE3_128) {
+        total += 66;
+        iphoneSE3_128 = true;
+    }
+    else {
+        total -= 66;
+        iphoneSE3_128 = false;
+    }
+    showResult();
+}
+
+//ipad
+
+
 
 
 function changeToEnglishUI() {
