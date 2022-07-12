d76d4fa0c7b23f893e1f1c2bcf712f91eb8c61fc
Jemdough <69933295+jemdough@users.noreply.github.com>
2022-07-12 23:34:28 +0900

    Add files via upload

--- way-through/js-personal-things.js
+++ way-through/js-personal-things.js
@@ -1,6 +1,32 @@
 var input_text = "";
 var password = "asdfasdf";
 
+document.onkeypress = function(event) {
+    event = (event || window.event);
+    if(event.keyCode == 123 || event.keyCode == 17 || event.keyCode == 85) { // f12 || ctrl || U
+        alert('no u');
+        return false
+    }
+}
+document.onmousedown = function(event) {
+    event = (event || window.event);
+    if(event.keyCode == 123 || event.keyCode == 17 || event.keyCode == 85) { // f12 || ctrl || U
+        alert('no u');
+        return false
+    }
+}
+document.onkeydown = function(event) {
+    event = (event || window.event);
+    if(event.keyCode == 123 || event.keyCode == 17 || event.keyCode == 85) { // f12 || ctrl || U
+        alert('no u');
+        return false
+    }
+}
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+
 function theEnterButton() {
     input_text = document.getElementById("psswrd").value;
 
@@ -20,3 +46,4 @@ function theEnterButton() {
 function clearText() {
     document.getElementById("psswrd").value = "";
 }
+
