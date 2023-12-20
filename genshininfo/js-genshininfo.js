5acb223d6005ece4e62683f49c2e6b703a173de4
Jemdou <shinaya0207@gmail.com>
2023-12-20 08:53:39 -0500

    Changes to genshinInfo
    Changed format of few html files, changed genshinInfo's html file,
    removed and added images of genshin characters

--- genshininfo/js-genshininfo.js
+++ genshininfo/js-genshininfo.js
@@ -1,1 +1,18 @@
-//temp
+/**
+ * Toggles a div to be shown or hidden
+ * @param {*} divID The id of the div or span that will be hidden or shown
+ * @param {*} linkID The id of the link that will be clicked to hide or show the div or span
+ * @param {*} linkName The name of the link that will be clicked on
+ */
+function toggleDiv(divID, linkID, linkName) {
+    var div = document.getElementById(divID);
+    var link = document.getElementById(linkID);
+
+    if(div.style.display == 'none') {
+        div.style.display = '';
+        link.innerText = '- ' + linkName;
+    } else {
+        div.style.display = 'none';
+        link.innerText = '+ ' + linkName;
+    }
+}
