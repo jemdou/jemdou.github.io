86fa84622c625ff5ce03633faa86eaf1cb4cdeec
Jemdou <shinaya0207@gmail.com>
2024-01-26 21:03:51 -0500

    Added functionality of choosing Character Banners in Genshin Wish Simulator
    Added functionality of choosing Character Banners in Genshin Wish Simulator
    - 4.0 ~ 4.3 Banners
    - 4 star characters will be able to be pulled related to their release date
    - ⭐️ means they are a newly introduced character
    - Changed the random number generatated from a float to an integer by rounding up the decimal points. (makes it harder to get characters)
    - Changed a bit of text alignment
    - Able to see how much primos and money you have spent. (calculation method listed on page)
    - Able to see current 5 star and 4 star character lineup on page

--- genshininfo/genshininfo-wishpuller/js-genshininfo-wishpuller.js
+++ genshininfo/genshininfo-wishpuller/js-genshininfo-wishpuller.js
@@ -1,6 +1,6 @@
 const maxFiveStarHardPity = 90; // 5성 천장
 const maxFiveStarSoftPity = 74; // 5성 활률 올라가는 때
-const maxFourStarHardPity = 10; // 4성 천장(?)
+const maxFourStarHardPity = 10; // 4성 천장
 const maxFourStarSoftPity = 9;
 
 var currFiveStar = 0; // 5성 천장까지의 뽑은 횟수
@@ -15,24 +15,17 @@ var percentWeaponFourStar = 2.55;
 var halfFiveStar = false; // 5성 필업 캐릭터 반천장 되었는지 안돼었는지
 var halfFourStar = false; // 4성 픽업 캐릭터 반천장 되었는지 안돼었는지
 
+var choosePickUp = false;
+
 var currNumWishes = 0; // 현재까지 뽑은 횟수
-var usedPrimo = currNumWishes * 1600; // 현재까지 원석 사용한 량
+var usedPrimo = currNumWishes * 160; // 현재까지 원석 사용한 량
+var usedMoney = usedPrimo * 14.8; 
 
 var randNum = 0; // 확률을 위한 랜덤한 숫자 (0 ~ 1)
 var itemText = ""; // 아이템의 이름을 이곳에 저장함
 
 var getFiveStar = false; // 5성을 얻었으면 true
 var getFourStar = false; // 4성을 얻었으면 true
-
-var characterNormalFiveStar = [
-    "진",
-    "다이루크",
-    "모나",
-    "각청",
-    "치치",
-    "타이나리",
-    "데히야"
-];
 
 var characterPickUpFiveStar = [
     "벤티",
@@ -69,58 +62,67 @@ var characterPickUpFiveStar = [
     "푸리나",
     "나비야"
 ];
-var charactersFourStar = [
-    "베넷",
-    "설탕",
-    "행추",
-    "향릉",
-    "연비",
-    "시노부",
-    "디오나",
-    "로자리아",
-    "피슬",
-    "운근",
-    "요요",
-    "고로",
-    "쿠죠 사라",
-    "파루잔",
-    "레일라",
-    "레이저",
-    "북두",
-    "미카",
-    "토마",
-    "콜레이",
-    "카베",
-    "케이야",
-    "노엘",
-    "바바라",
-    "응광",
-    "중운",
-    "사유",
-    "사카노인 헤이조",
-    "도리",
-    "캔디스",
-    "엠버",
-    "리사",
-    "구라구라꽃 (신염)",
-    "키라라",
-    "리넷",
-    "프레미네",
-    "샤를로트",
-    "슈브르즈"
+
+var characterNormalFiveStar = [];
+var characterNormalFiveStarBase = [
+    "진",
+    "다이루크",
+    "모나",
+    "각청",
+    "치치", // 1.0 size 5
+    "타이나리", // 3.0 size 6
+    "데히야" // 3.5 size 7
 ];
+
+var charactersFourStar = [];
+var charactersFourStarBase = [
+    "엠버",
+    "바바라",
+    "북두",
+    "베넷",
+    "행추",
+    "중운",
+    "피슬",
+    "케이야",
+    "리사",
+    "응광",
+    "노엘",
+    "레이저",
+    "설탕",
+    "향릉",
+    "행추", // 1.0, size : 15
+    "디오나",
+    "구라구라꽃(신염)", // 1.1  size : 17
+    "로자리아", // 1.4 size : 18
+    "연비", // 1.5 size : 19
+    "사유", // 2.0 size : 20
+    "쿠죠 사라", // 2.1 size : 21
+    "토마", // 2.2 size : 22
+    "고로", // 2.3 size : 23
+    "운근", // 2.4 size : 24
+    "쿠키 시노부", // 2.7 size : 25
+    "시카노인 헤이조", // 2.8 size : 26
+    "도리",
+    "콜레이", // 3.0 size : 28
+    "캔디스", // 3.1 size : 29
+    "레일라", // 3.2 size : 30
+    "파루잔", // 3.3 size : 31
+    "요요", // 3.4 size : 32
+    "미카", // 3.5 size : 33
+    "카베", // 3.6 size : 34
+    "키라라", // 3.7 size : 35
+    "리넷", // 4.0 전반 size : 36
+    "프레미네", // 4.0 후반 size : 37
+    "샤를로트", // 4.2 size : 38
+    "슈브르즈", // 4.3 size : 39
+]
+
 var weaponFourStar = [
     "4성 무기"
 ]
 
-var currPickUpFiveStar = "라이덴 쇼군";
-var currPickUpFourStar = [
-    "베넷",
-    "슈브르즈",
-    "쿠죠 사라"
-];
-
-
+var currPickUpFiveStar = "";
+var currPickUpFourStar = [];
 
 function percentFiveStarCalc() {
     if(maxFiveStarSoftPity <= currFiveStar && currFiveStar <= (maxFiveStarHardPity + 15)) {
@@ -145,11 +147,18 @@ function getRandomNum(min, max) {
 }
 
 function newWishOne() {
+    if(choosePickUp == false) {
+        window.alert("픽업 배너를 선택해주세요!");
+        return;
+    }
+
     currNumWishes += 1;
     currFourStar += 1;
     currFiveStar += 1;
     keepFiveStar += 1;
     randNum = getRandomNum(0, 100);
+    // randNum = randNum.toFixed(1);
+    randNum = Math.ceil(randNum);
 
     percentFiveStarCalc();
     percentFourStarCalc();
@@ -208,6 +217,11 @@ function newWishOne() {
 }
 
 function newWishTen() {
+    if(choosePickUp == false) {
+        window.alert("픽업 배너를 선택해주세요!");
+        return;
+    }
+
     for(let cnt = 0; cnt < 10; cnt++) {
         newWishOne();
     }
@@ -224,6 +238,9 @@ function endFunction() {
         currFourStar = 0;
         percentFourStar = 5.1;
     }
+    if(maxFourStarHardPity - currFourStar == 0) {
+        currFourStar--;
+    }
 }
 
 function printInfo(item) {
@@ -231,9 +248,348 @@ function printInfo(item) {
     str = item + " 획득<br>" + str;
     document.getElementById("pullbox").innerHTML = str;
 
-    document.getElementById("pullcount").innerHTML = "총 기원 횟수 : " + currNumWishes;
-    document.getElementById("fivestarpitty").innerHTML = "5성 천장까지의 남은 횟수 : " + (maxFiveStarHardPity - currFiveStar);
-    document.getElementById("fourstarpitty").innerHTML = "4성 천장까지의 남은 횟수 : " + (maxFourStarHardPity - currFourStar);
+    document.getElementById("pullcount").innerHTML = "총 기원 횟수 : " + currNumWishes + "회";
+    usedPrimo = currNumWishes * 160;
+    document.getElementById("primocount").innerHTML = "총 사용한 원석 개수 : " + usedPrimo + "개";
+    usedMoney = usedPrimo * 14.8;
+    document.getElementById("moneycount").innerHTML = "총 사용한 돈 : " + usedMoney + "원";
+    document.getElementById("fivestarpitty").innerHTML = "5성 천장까지의 남은 횟수 : " + (maxFiveStarHardPity - currFiveStar) + "회";
+    document.getElementById("fourstarpitty").innerHTML = "4성 천장까지의 남은 횟수 : " + (maxFourStarHardPity - currFourStar) + "회";
     
+}
+
+function setNames(date) {
+    document.getElementById("dateofevent").innerHTML = "픽업 기간 : " + date;
+    document.getElementById("fivestarname").innerHTML = "5성 픽업 캐릭터 : " + currPickUpFiveStar;
+    document.getElementById("fourstarnames").innerHTML = "4성 픽업 캐릭터 : " + currPickUpFourStar[0] + ", " + currPickUpFourStar[1] + ", " + currPickUpFourStar[2];
+}
+
+/*=== banner functions ===*/
+function NameVersionTemplate() {
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7; // change according to version
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 39; // change according to version
+
+    currPickUpFiveStar = "5성픽업캐릭";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("4성픽업캐릭1");
+    currPickUpFourStar.push("4성픽업캐릭2");
+    currPickUpFourStar.push("4성픽업캐릭3");
+
+    window.alert("버전 전/후반 픽업캐릭 픽업으로 설정되었습니다!");
+}
+
+
+function Raiden43() { // 4.3 후반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 39;
+
+    currPickUpFiveStar = "라이덴 쇼군";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("베넷");
+    currPickUpFourStar.push("슈브르즈");
+    currPickUpFourStar.push("쿠죠 사라");
+
+    setNames("2024년 1월 9일 ~ 2024년 1월 30일");
+
+    window.alert("4.3 후반 라이덴 쇼군 픽업으로 설정되었습니다!");
+}
+function Yoimiya43() { // 4.3 후반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 39;
+
+    currPickUpFiveStar = "요이미야";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("베넷");
+    currPickUpFourStar.push("슈브르즈");
+    currPickUpFourStar.push("쿠죠 사라");
+
+    setNames("2024년 1월 9일 ~ 2024년 1월 30일");
+
+    window.alert("4.3 후반 요이미야 픽업으로 설정되었습니다!");
+}
+function Navia43() { // 4.3 전반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 38;
+
+    currPickUpFiveStar = "나비아";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("캔디스");
+    currPickUpFourStar.push("로자리아");
+    currPickUpFourStar.push("설탕");
+
+    setNames("2023년 12월 20일 ~ 2024년 1월 9일");
+
+    window.alert("4.3 전반 나비아 픽업으로 설정되었습니다!");
+}
+function Ayaka43() { // 4.3 전반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 38;
+
+    currPickUpFiveStar = "카미사토 아야카";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("캔디스");
+    currPickUpFourStar.push("로자리아");
+    currPickUpFourStar.push("설탕");
+
+    setNames("2023년 12월 20일 ~ 2024년 1월 9일");
+
+    window.alert("4.3 전반 카미사토 아야카 픽업으로 설정되었습니다!");
+}
+function Cyno42() { // 4.2 후반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 38;
+
+    currPickUpFiveStar = "사이노";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("키라라");
+    currPickUpFourStar.push("쿠키 시노부");
+    currPickUpFourStar.push("향릉");
+
+    setNames("2023년 11월 28일 ~ 2023년 12월 19일");
+
+    window.alert("4.2 후반 사이노 픽업으로 설정되었습니다!");
+}
+function Ayato42() { // 4.2 후반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 38;
+
+    currPickUpFiveStar = "카미사토 아야토";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("키라라");
+    currPickUpFourStar.push("쿠키 시노부");
+    currPickUpFourStar.push("향릉");
+
+    setNames("2023년 11월 28일 ~ 2023년 12월 19일");
+
+    window.alert("4.2 후반 카미사토 아야토 픽업으로 설정되었습니다!");
+}
+function Furina42() { // 4.2 전반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 38;
+
+    currPickUpFiveStar = "푸리나";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("샤를로트");
+    currPickUpFourStar.push("콜레이");
+    currPickUpFourStar.push("북두");
+
+    setNames("2023년 11월 8일 ~ 2023년 11월 28일");
+
+    window.alert("4.2 전반 푸리나 픽업으로 설정되었습니다!");
+}
+function Baizhu42() { // 4.2 전반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 38;
+
+    currPickUpFiveStar = "백출";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("샤를로트");
+    currPickUpFourStar.push("콜레이");
+    currPickUpFourStar.push("북두");
+
+    setNames("2023년 11월 8일 ~ 2023년 11월 28일");
+
+    window.alert("4.2 전반 백출 픽업으로 설정되었습니다!");
+}
+function Wriothesley41() { // 4.1 후반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 37;
+
+    currPickUpFiveStar = "라이오슬리";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("중운");
+    currPickUpFourStar.push("토마");
+    currPickUpFourStar.push("도리");
+
+    setNames("2023년 10월 17일 ~ 2023년 11월 7일");
+
+    window.alert("4.1 후반 라이오슬리 픽업으로 설정되었습니다!");
+}
+function Venti41() { // 4.1 후반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 37;
+
+    currPickUpFiveStar = "벤티";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("중운");
+    currPickUpFourStar.push("토마");
+    currPickUpFourStar.push("도리");
+
+    setNames("2023년 10월 17일 ~ 2023년 11월 7일");
+
+    window.alert("4.1 후반 벤티 픽업으로 설정되었습니다!");
+}
+function Hutao41() { // 4.1 전반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 37;
+
+    currPickUpFiveStar = "호두";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("행추");
+    currPickUpFourStar.push("피슬");
+    currPickUpFourStar.push("디오나");
+
+    setNames("2023년 9월 27일 ~ 2023년 10월 17일");
+
+    window.alert("4.1 전반 호두 픽업으로 설정되었습니다!");
+}
+function Neuvillette41() { // 4.1 전반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 37;
+
+    currPickUpFiveStar = "느비예트";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("행추");
+    currPickUpFourStar.push("피슬");
+    currPickUpFourStar.push("디오나");
+
+    setNames("2023년 9월 27일 ~ 2023년 10월 17일");
+
+    window.alert("4.1 전반 느비예트 픽업으로 설정되었습니다!");
+}
+function Zhongli40() { // 4.0 후반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 37;
+
+    currPickUpFiveStar = "종려";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("프레미네");
+    currPickUpFourStar.push("사유");
+    currPickUpFourStar.push("노엘");
+
+    setNames("2023년 9월 5일 ~ 2023년 9월 26일");
+
+    window.alert("4.0 후반 종려 픽업으로 설정되었습니다!");
+}
+function Tartaglia40() { // 4.0 후반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 37;
+
+    currPickUpFiveStar = "타르탈리아";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("프레미네");
+    currPickUpFourStar.push("사유");
+    currPickUpFourStar.push("노엘");
+
+    setNames("2023년 9월 5일 ~ 2023년 9월 26일");
+
+    window.alert("4.0 후반 타르탈리아 픽업으로 설정되었습니다!");
+}
+function Yelan40() { // 4.0 전반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 36;
+
+    currPickUpFiveStar = "야란";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("리넷");
+    currPickUpFourStar.push("베넷");
+    currPickUpFourStar.push("바바라");
+
+    setNames("2023년 8월 16일 ~ 2023년 9월 5일");
+
+    window.alert("4.0 전반 야란 픽업으로 설정되었습니다!");
+}
+function Lyney40() { // 4.0 전반
+    choosePickUp = true;
+
+    characterNormalFiveStar = characterNormalFiveStarBase;
+    characterNormalFiveStar.length = 7;
+    charactersFourStar = charactersFourStarBase;
+    charactersFourStar.length = 36;
+
+    currPickUpFiveStar = "리니";
+    currPickUpFourStar.length = 0;
+    currPickUpFourStar.push("리넷");
+    currPickUpFourStar.push("베넷");
+    currPickUpFourStar.push("바바라");
+
+    setNames("2023년 8월 16일 ~ 2023년 9월 5일");
+
+    window.alert("4.0 전반 리니 픽업으로 설정되었습니다!");
+}
+
+
+/*=== toggling divs ===*/
+/**
+ * Toggles a div to be shown or hidden
+ * @param {*} divID The id of the div or span that will be hidden or shown
+ * @param {*} linkID The id of the link that will be clicked to hide or show the div or span
+ * @param {*} linkName The name of the link that will be clicked on
+ */
+function toggleDiv(divID, linkID, linkName) {
+    var div = document.getElementById(divID);
+    var link = document.getElementById(linkID);
+
+    if(div.style.display == 'none') {
+        div.style.display = '';
+        link.innerText = '- ' + linkName;
+    } else {
+        div.style.display = 'none';
+        link.innerText = '+ ' + linkName;
+    }
 }
 
